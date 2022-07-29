4c6f9b01e9087b685b37148704336edeb74e3616
Olga Chernega <chernega.o@gmail.com>
2022-07-29 16:58:01 +0300

    feat: task solution found and added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+        let num = Math.abs(n).toString();
+        let i = num.length - 1;
+        let result = '';
+        while (i >= 0) {
+          result = `${result}${num[i]}`;
+          i = i - 1;
+        }
+        return result;
+      };
